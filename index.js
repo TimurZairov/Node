9a38e9682bb219c65a0f54077e0dfc8e32af1bfa
Timur <zairovne@gmail.com>
2022-11-19 17:44:04 +0500

    script nodemon для обновления сервера

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
 
  res.write('<h1>Hello</h1>')
  res.write('<h2>Hello</h2>')
- res.write('<h>Hello</h3>')
+ res.write('<h>Hello!!!!</h3>')
  res.end(`
   <div style="background: aquamarine; width: 200px; height: 200px"><h1>TEst</h1></div>
  `)
