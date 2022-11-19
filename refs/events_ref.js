c5fafa0d120645b93b84bb332136edfed7cb3350
Timur <zairovne@gmail.com>
2022-11-19 16:39:34 +0500

    Прослушивание определенных событий EventEmitter = events

---
+++ refs/events_ref.js
@@ -0,0 +1,17 @@
+// Что такое EventEmitter => прослушивание определенных событий
+const EventEmitter = require('events')
+
+
+class Logger extends EventEmitter {
+    log(message) {
+        this.emit('message', `${message} ${Date.now()}` )
+    }
+}
+
+const logger = new Logger()
+
+logger.on('message', data => {
+    console.log(data)
+})
+
+logger.log('Hello')
