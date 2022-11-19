a7035e128bae7539d7c7e7ac81062a3e41f8ce47
Timur <zairovne@gmail.com>
2022-11-19 13:00:53 +0500

    Изменение имени фала fs.rename

--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -15,27 +15,52 @@ const path = require('path')
 
 //создание файла
 
-fs.writeFile(
+// fs.writeFile(
+//     path.join(__dirname, 'notes', 'mynotes.txt'),
+//     'Hello World',
+//     (err) => {
+//         if (err) {
+//             throw new Error(err)
+//         }
+//         console.log("Файл был создан")
+//
+//         // изменение созданного файла без перетирания
+//         fs.appendFile(
+//             path.join(__dirname, 'notes', 'mynotes.txt'),
+//             " From append file",
+//             (err) => {
+//                 if (err) {
+//                     throw new Error(err)
+//                 }
+//
+//                 console.log('Файл был изменен')
+//
+//             }
+//         )
+//     }
+// )
+//
+// //прочитать файл c BUFFER
+//
+// fs.readFile(
+//     path.join(__dirname, 'notes', 'mynotes.txt'),
+//     'utf-8',
+//     //принимает data
+//     (err, data) => {
+//         if (err) throw new Error(err)
+//         console.log(data)
+//     }
+// )
+
+
+//переименовать файл
+
+
+fs.rename(
     path.join(__dirname, 'notes', 'mynotes.txt'),
-    'Hello World',
+    path.join(__dirname, 'notes', 'notes.txt'),
     (err) => {
-        if (err) {
-            throw new Error(err)
-        }
-        console.log("Файл был создан")
-
-        // изменение созданного файла без перетирания
-        fs.appendFile(
-            path.join(__dirname, 'notes', 'mynotes.txt'),
-            " From append file",
-            (err) => {
-                if (err) {
-                    throw new Error(err)
-                }
-
-                console.log('Файл был изменен')
-
-            }
-        )
+        if (err) throw new Error(err)
+        console.log('Имя файла бфло изменено')
     }
 )
