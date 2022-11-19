53c8fe580a7f4c6c3cd51afeea2211e35a78c35e
Timur <zairovne@gmail.com>
2022-11-19 16:55:45 +0500

    http => создание сервера с помощью http

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
- const userObj = require('./user')
+const http = require('http')
 
-console.log(userObj.user)
+const server = http.createServer((req, res) => {
+ console.log(req.url)
 
- userObj.sayHello()
+
+ res.write('<h1>Hello</h1>')
+ res.write('<h2>Hello</h2>')
+ res.write('<h>Hello</h3>')
+ res.end(`
+  <div style="background: aquamarine; width: 200px; height: 200px"><h1>TEst</h1></div>
+ `)
+})
+
+server.listen(3000, () => {
+ console.log('Server is started')
+})
